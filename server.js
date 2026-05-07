4302215810398bafd8aa75424cfc4fbcb7cb1604
jonadab13 <jonadabtekie13@gmail.com>
2026-05-07 09:17:16 +0200

    Added Scores Database and server is up and running

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-const e = require("express");
 const express = require("express");
 const sqlite3 = require("sqlite3");
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
+
+const db = new sqlite3.Database("./scores.db");
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS scores (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    playerName TEXT NOT NULL,
+    score INTEGER NOT NULL
+  )
+`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
